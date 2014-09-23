68f31931970faff1111c41cdffcd434f8e49d573
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-09-23 18:42:04 -0300

    al parecer nunca cierra las conexiones a la base. asi que modifique el logger para que abra, y la cierre cuando loguea. es menos eficiente pero va a solucionar el problema.

--- cubieDevice/src/main/java/ar/com/dcsys/firmware/logging/SqlHandler.java
+++ cubieDevice/src/main/java/ar/com/dcsys/firmware/logging/SqlHandler.java
@@ -9,7 +9,6 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
 import ar.com.dcsys.persistence.JdbcConnectionProvider;
@@ -18,68 +17,64 @@ public class SqlHandler extends Handler {
 
 	private static final Logger logger = Logger.getLogger(SqlHandler.class.getName());
 	private JdbcConnectionProvider cp;
-	private Connection con;
-	private PreparedStatement addLog;
 	
 	@Inject
 	public SqlHandler(JdbcConnectionProvider cp) {
 		this.cp = cp;
 	}
-
+	
 	@PostConstruct
 	private void init() {
 		try {
-			this.con = cp.getConnection();
-			PreparedStatement st = this.con.prepareStatement("create table if not exists logs "
-																	+ "(created timestamp not null default now(),"
-																	+ "source varchar not null,"
-																	+ "log varchar not null)");
+			Connection con = cp.getConnection();
 			try {
-				st.execute();
+				PreparedStatement st = con.prepareStatement("create table if not exists logs "
+																		+ "(created timestamp not null default now(),"
+																		+ "source varchar not null,"
+																		+ "log varchar not null)");
+				try {
+					st.execute();
+				} finally {
+					st.close();
+				}
 			} finally {
-				st.close();
+				con.close();
 			}
 
-			addLog = con.prepareStatement("insert into logs (log,source) values (?,?)");
-			
 		} catch (SQLException e) {
 			logger.log(Level.SEVERE,e.getMessage(),e);
 		}
 	}
 	
-	@PreDestroy
 	@Override
 	public void close() throws SecurityException {
-
-		try {
-			if (addLog != null) {
-				addLog.close();
-				addLog = null;
-			}
-		} catch (SQLException e) {
-			throw new SecurityException(e.getMessage());
-		}
-
-		try {
-			if (con != null) {
-				con.close();	
-				con = null;
-			}
-		} catch (SQLException e) {
-			throw new SecurityException(e.getMessage());
-		}
+		// TODO Auto-generated method stub
+		
 	}
-
+	
 	@Override
 	public void flush() {
 	}
 
 	@Override
 	public void publish(LogRecord l) {
+		
 		try {
-			addLog.setString(1, l.getMessage());
-			addLog.setString(2, l.getSourceClassName() + " " + l.getSourceMethodName());
-			addLog.execute();
+			Connection con = cp.getConnection();
+			try {
+				PreparedStatement addLog = con.prepareStatement("insert into logs (log,source) values (?,?)");
+				try {
+					addLog.setString(1, l.getMessage());
+					addLog.setString(2, l.getSourceClassName() + " " + l.getSourceMethodName());
+					addLog.execute();
+					
+				} finally {
+					addLog.close();
+				}
+				
+			} finally {
+				con.close();
+			}
 			
 		} catch (Exception e) {
 			logger.log(Level.SEVERE,e.getMessage(),e);
