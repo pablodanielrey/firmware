e86acde608c4612c0f9578942c0bf8568565f722
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-01-20 16:28:19 -0300

    agregue el modulo de camabio. y agregue el script para prender el led.

---
+++ cubieDevice/doc/scripts/fingerprint/camabio.py
@@ -0,0 +1,27 @@
+
+""" setea el checksum en el paquete de datos """
+def calcChksum(data):
+    sum = 0
+    for i in range(len(data) - 2):
+        sum = sum + data[i]
+    return sum
+
+""" setea el checksum en el paquete de datos """
+def setChksum(data):
+    sum = calcChksum(data)
+    data[len(data) - 2] = (sum & 0xff)
+    data[len(data) - 1] = ((sum & 0xff00) >> 8)
+
+
+""" chequea que el chksum del paquete este ok """
+def verifyChksum(data):
+    sum = calcChksum(data)
+    l = len(data)
+    sump = ((data[l - 1] << 8) + data[l - 2])
+    print('suma calculada %s' % sum)
+    print('suma del paquete %s' % sump)
+    return (sum == sump)
+
+def printArray(data):
+    string = ''.join(format(x,'02x') for x in data)
+    print(string)
---
+++ cubieDevice/doc/scripts/fingerprint/sensorLedControl.py
@@ -0,0 +1,23 @@
+import array
+import serial
+import camabio
+
+on = 0x01
+data = [0x55,0xaa,0x42,0x01,0x02,0x0,on,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x00,0x00]
+camabio.setChksum(data)
+
+print('abriendo puerto seriel')
+ser = serial.Serial("/dev/ttyS1",115200,timeout=5)
+
+print('escribiendo bytes en el puerto serie')
+camabio.printArray(data)
+setBauds = array.array('B', data).tostring()
+ser.write(setBauds);
+ser.flush()
+
+print('tratando de leer bytes desde el puerto serie: ')
+data2 = ser.read(len(data))
+if data2 == None:
+    print('No se leyo ningun byte')
+else:
+    camabio.printArray(data2)
