d0aefe2690c9d745f34e42e186187099cf361ce2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-01-20 14:36:25 -0300

    agregue para que sea configurable el puerto e ip del websocket que escucha para recibir los comandos.

---
+++ cubieDevice/src/main/java/ar/com/dcsys/firmware/websocket/WebsocketServerData.java
@@ -0,0 +1,39 @@
+package ar.com.dcsys.firmware.websocket;
+
+import javax.inject.Inject;
+
+import ar.com.dcsys.config.Config;
+
+public class WebsocketServerData {
+
+	@Inject @Config String ip;
+	@Inject @Config String port;
+	@Inject @Config String url;
+	
+	public String getIp() {
+		return ip;
+	}
+	
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	public int getPort() {
+		return Integer.parseInt(port);
+	}
+	
+	public void setPort(int port) {
+		this.port = String.valueOf(port);
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	
+	
+}
