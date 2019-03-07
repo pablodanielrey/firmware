0d0e3fddc431a5a9515f7ffe6974f6aafd4e5362
Jano <oportoalejandroa@gmail.com>
2019-03-07 15:32:56 -0300

    resolvimos el punto 2 del problema 3

--- scripts/problema3/publish.py
+++ scripts/problema3/publish.py
@@ -1,7 +1,12 @@
 import paho.mqtt.publish as publish
+import json
+
+dispositivo={"Estado":"Online", "Encendido":"OFF","IP":"16.16.16.16"}
+
+
 
 servidor = '169.254.254.254'
-payload = 'hola'
+payload = json.dumps(dispositivo)
 topico = 'prueba/topico'
 
 publish.single(topico, payload, hostname=servidor)
--- scripts/problema3/subscribe.py
+++ scripts/problema3/subscribe.py
@@ -1,12 +1,16 @@
 import paho.mqtt.subscribe as subscribe
-
+import json
 import logging
 logging.getLogger().setLevel(logging.INFO)
 
 def on_mqtt(client, userdata, message):
-    logging.info(message.topic)
+    # logging.info(message.topic)
+    # logging.info(type(message.payload))
     logging.info(message.payload.decode('UTF-8'))
-    pass
+    # logging.info(type(message.payload.decode('UTF-8')))
+    a = message.payload.decode('UTF-8')
+    x = json.loads(a)
+    logging.info(type(x))
 
 topico = 'prueba/topico'
 servidor = '169.254.254.254'
