c38a94b9c2787397f4d31914ec5fc5098bc1c41e
Jano <oportoalejandroa@gmail.com>
2019-03-15 18:31:57 -0300

    p4 del problema  4 avanzado, pero aun no esta resuelto

--- scripts/problema4/script2.py
+++ scripts/problema4/script2.py
@@ -12,6 +12,8 @@ topicoprograma = re.compile(r"puerta\/(.*)") #puerta/ditesi
 payloadprograma = re.compile(r"\{\s*\"(.*)\"\s*:\s*\"(.*)\"\s*}") #ditesi = {sys.argv[1]:sys.argv[2]}
 
 def manejar_cerradura(oficina, personaAccion):
+    logging.info('----Manejar cerradura----')
+    personaactual = {}
     if oficina not in Office:
         logging.info(f'{oficina} no existe, la creo')
         Office[oficina] = {}
@@ -22,17 +24,22 @@ def manejar_cerradura(oficina, personaAccion):
         if persona not in personasenoficina:
             logging.info(f'{persona} no existe, lo agrego')
             personasenoficina[persona] = False
+            personaactual[persona] = False
         if accion == 'abrir':
-            if personasenoficina[persona] == True: #me fijo si esta adentro
-                personasenoficina[persona] = False
-                po = json.dumps(personasenoficina)
-                publish.single(f"ditesi/asistencia", po, hostname="169.254.254.254")
-            elif personasenoficina[persona] == False: #me fijo si esta afuera
+            if personasenoficina[persona] == False: #me fijo si esta afuera
                 personasenoficina[persona] = True
-                po = json.dumps(personasenoficina)
-                publish.single(f"ditesi/asistencia", po, hostname="169.254.254.254")
+                personaactual[persona] = True
+                po = json.dumps(personaactual)
+                logging.info(po)
+                publish.single("ditesi/asistencia", po, hostname="169.254.254.254")
+            elif personasenoficina[persona] == True: #me fijo si esta adentro
+                personasenoficina[persona] = False
+                personaactual[persona] = False
+                po = json.dumps(personaactual)
+                logging.info(po)
+                publish.single("ditesi/asistencia", po, hostname="169.254.254.254")
+    logging.info('----Offcie----')
     logging.info(Office)
-
 
 def on_mqtt(client, userdata, message):
     try:
--- scripts/problema4/script3.py
+++ scripts/problema4/script3.py
@@ -6,7 +6,8 @@ import re
 import logging
 logging.getLogger().setLevel(logging.INFO)
 
-Office = {}
+sonoffs = {}
+
 
 dispositivos = {
     'jano':['janouno'],
@@ -15,32 +16,69 @@ dispositivos = {
 }
 
 
-topico_entrar_salir = ...
-payload_entrar_salir = ...
-topico_estado_sonoff = ...
-payload_estado_sonoff = ...
+topico_entrar_salir = re.compile(r"(.*)/asistencia")
+topico_estado_sonoff = re.compile(r".*/(.*)/RESULT")
+payload_estado_sonoff = re.compile(r"\{\s*\"POWER\"\s*:\s*\"(.*)\"\s*}")
 
 def manejar_entrar_salir(persona, accion):
+    logging.info('----Entrar / Salir----')
     """ controlar si la persona entro o salio y en base a eso """
     """ envia_mqtt_mensaje_apagar_prender """
-    pass
+    if accion:
+        logging.info(f'{persona} entro a la oficina')
+        if persona in dispositivos.keys():
+            logging.info(f'{persona} tiene asignado un dispositivo')
+            for sonoff in dispositivos[persona]:
+                if sonoff not in sonoffs.keys():
+                    publish.single(f"cmnd/{sonoff}/power", "ON", hostname="169.254.254.254")
+                elif not sonoffs[sonoff]:
+                    publish.single(f"cmnd/{sonoff}/power", "ON", hostname="169.254.254.254")
+        else:
+            logging.info(f'{persona} NO tiene asignado un dispositivo')
+    else:
+        logging.info(f'{persona} salio de la oficina')
+        if persona in dispositivos.keys():
+            logging.info(f'{persona} tiene asignado un dispositivo')
+            for sonoff in dispositivos[persona]:
+                if sonoff not in sonoffs.keys():
+                    publish.single(f"cmnd/{sonoff}/power", "OFF", hostname="169.254.254.254")
+                elif not sonoffs[sonoff]:
+                    logging.info(f'{persona} tiene asignado un dispositivo')
+                    publish.single(f"cmnd/{sonoff}/power", "OFF", hostname="169.254.254.254")
 
+        else:
+            logging.info(f'{persona} NO tiene asignado un dispositivo')
 
-def manejar_estado_sonoff(sonoff, estado):
-    pass
+def guardar_estado_sonoff(sonoff, estado):
+    sonoffs[sonoff] = estado
+
+    logging.info(sonoffs)
+
+    # publish.single(f"cmnd/{sonoff}/power", f"{estado}", hostname="169.254.254.254")
 
 
 def on_mqtt(client, userdata, message):
     try:
+        match_topico_entrar_salir = topico_entrar_salir.match(message.topic)
+        match_topico_estado_sonoff = topico_estado_sonoff.match(message.topic)
+        match_payload_estado_sonoff = payload_estado_sonoff.match(message.payload.decode('UTF-8'))
 
-        manejar_entrar_salir()
+        if match_topico_entrar_salir:
+            personal = message.payload
+            personas = json.loads(personal)
+            persona = list(personas.keys())[0]
+            accion = personas[persona]
+            manejar_entrar_salir(persona, accion)
 
-        manejar_estado_sonoff()
+        if match_topico_estado_sonoff:
+            sonoff = match_topico_estado_sonoff.group(1)
+            estado = match_payload_estado_sonoff.group(1)
+            guardar_estado_sonoff(sonoff, estado)
+
 
     except Exception as ex:
         logging.exception(ex)
 
-# topic = 'puerta/ditesi'
-topic = []
+topic = ['ditesi/asistencia', 'stat/#']
 servidor = '169.254.254.254'
 subscribe.callback(on_mqtt, topic, hostname=servidor)
