692260921e53425873837a59c6e2d502ccdf0f60
Jano <oportoalejandroa@gmail.com>
2019-03-18 13:09:59 -0300

    Configuracion de los archivos para conectar con VSC

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -v /src/github/firmware/scripts:/scripts -p 5678:5678 hardware:latest bash
+docker run --rm -ti -v /src/github/firmware/scripts:/scripts -p 5678:5678 -p 5679:5679 -p 5680:5680 --name scripts hardware:latest bash
