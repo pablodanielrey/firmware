e0d80a94446d8e01891095048e61fd9d79f8d89f
Jano <oportoalejandroa@gmail.com>
2019-03-20 13:19:16 -0300

    corregimos el docker file para que levante el entrypoint el script2

--- docker-script2/ejecutar.sh
+++ docker-script2/ejecutar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -v /src/github/firmware/scripts:/scripts -p 5681:5681 --name scriptss script2:latest bash
+docker run --rm -d -v /src/github/firmware/scripts:/scripts -p 5681:5681 --name scriptss script2:latest
