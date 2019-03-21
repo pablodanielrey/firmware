9cd94e6a7fd1c7774182a76d428dffa26583945a
Jano <oportoalejandroa@gmail.com>
2019-03-21 10:40:18 -0300

    Se modifico el archivo para pasarle como argumento el usuario que ingreso "ej: sh ejecutar.sh jano"

--- docker-script1/ejecutar.sh
+++ docker-script1/ejecutar.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -d -v /src/github/firmware/scripts:/scripts -p 5678:5678 --name scripts script1:latest
+echo $1
+docker run --rm -d -v /src/github/firmware/scripts:/scripts -p 5678:5678 --name scripts script1:latest $1
