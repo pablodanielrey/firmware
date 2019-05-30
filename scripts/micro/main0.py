e9979822fa0ba8a018522bcd7e827c8519424397
Jano <oportoalejandroa@gmail.com>
2019-05-30 19:01:21 -0300

    main

--- scripts/micro/main0.py
+++ scripts/micro/main0.py
@@ -9,22 +9,38 @@ topic_sub = "topico/prueba"
 topic_pub = "puerta/estado"
 led = Pin(15, Pin.OUT)
 
+#persona = ""
+
 c = MQTTClient(mqttClient, server)
+
 
 def sub_cb(topic, msg):
     print((topic, msg))
+    #los que llegan con la puerta cerrada - registra mensaje para abrir
+    print(type(msg))
+    msg_decode = msg.decode('UTF-8')
+    global persona
+    persona = msg_decode
+
+    return persona
+    
+def abrir_cerrar(persona):
+    #variable global para la ultima ejecución de abrir_cerrar
     led(1)
-    c.publish(topic_pub, msg)
+    c.publish(topic_pub, persona)
     time.sleep_ms(5000)
     led(0)
-    msg = "PUERTA CERRADA!!"
-    c.publish(topic_pub, msg)
-
+    persona = "PUERTA CERRADA!!"
+    c.publish(topic_pub, persona)
+    
 def subscriber(server):
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(topic_sub)
     while True:
-        c.wait_msg()
+        mensaje = c.wait_msg()
+        abrir_cerrar(persona)
+        # aca se llama a abrir
+        print(mensaje)
         print('despues de whait!')
     c.disconnect()
--- scripts/micro/main1.py
+++ scripts/micro/main1.py
@@ -1,7 +1,9 @@
 #Script para micropython
-import time
+import utime
 from umqttsimple import MQTTClient
 from machine import Pin
+
+persona = " "
 
 server="169.254.254.254"
 mqttClient = "umqtt_client"
@@ -9,44 +11,38 @@ topic_sub = "topico/prueba"
 topic_pub = "puerta/estado"
 led = Pin(15, Pin.OUT)
 
-#persona = "A"
-
 c = MQTTClient(mqttClient, server)
 
+puerta_abierta = False
+
+tiempo = utime.ticks_add(utime.ticks_ms(), -1000)
+ini = 0
 
 def sub_cb(topic, msg):
-    print((topic, msg))
-    #los que llegan con la puerta cerrada - registra mensaje para abrir
-    print(type(msg))
+    print("SUBCB")
     msg_decode = msg.decode('UTF-8')
     global persona
     persona = msg_decode
-    print("---------")
-    print(persona)
-    print(type(persona))
-    print("---------")
     return persona
     
 def abrir_cerrar(persona):
-    #variable global para la ultima ejecución de abrir_cerrar
+    print("ABRIR Y CERRAR")
     led(1)
     c.publish(topic_pub, persona)
-    time.sleep_ms(5000)
+    utime.sleep_ms(5000)
     led(0)
     persona = "PUERTA CERRADA!!"
     c.publish(topic_pub, persona)
-    print("---------")
-    print(type(persona))
-    print(persona)
-    print("---------")
     
 def subscriber(server):
+    print("SUBSCRIBIR")
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(topic_sub)
     while True:
-        c.wait_msg()
+        c.check_msg()
+        #if persona != " ":
         abrir_cerrar(persona)
-        # aca se llama a abrir
-        print('despues de whait!')
+        #persona = " "
+        print("FIN") 
     c.disconnect()
