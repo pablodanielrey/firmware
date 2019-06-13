7e3aa3081423f5af93824acff25fce688247a1d0
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-13 15:03:23 -0300

    agregue algunas modificaciones para organizar un poquito mas el codigo

--- scripts/Cerradura/micro/boot.py
+++ scripts/Cerradura/micro/boot.py
@@ -1,8 +1,6 @@
 #This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import connect
-import main
 import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
@@ -10,7 +8,6 @@ import webrepl
 webrepl.start()
 gc.collect()
 
-
-if __name__ == "__main__":
-    connect.conectar_wifi()
-    main.subscriber(server)
+#esto es el programa principal
+import main
+main.ejecutar()
--- scripts/Cerradura/micro/main.py
+++ scripts/Cerradura/micro/main.py
@@ -1,60 +1,6 @@
-#Script para micropython
-from umqttsimple import MQTTClient
-from machine import Pin
-from machine import Timer
+import connect
+import cerradura
 
-persona = " "
-
-server="169.254.254.254"
-mqttClient = "umqtt_client"
-topic_sub = "topico/prueba"
-topic_pub = "puerta/estado"
-led = Pin(15, mode=Pin.OUT)
-c = MQTTClient(mqttClient, server)
-
-puerta_cerrada = True
-tiempo = Timer(-1)
-
-def sub_cb(topic, msg):
-    global persona
-    persona = msg.decode('UTF-8')[19:]
-    print("***********")
-    print(persona)
-
-def abrir_cerrar(persona):
-    global puerta_cerrada
-    print("puerta_cerrada") 
-    print(puerta_cerrada)
-    if puerta_cerrada:
-        puerta_cerrada=False        
-        led(1)
-        c.publish(topic_pub, persona)
-        print("puerta_cerrada") 
-        print(puerta_cerrada)
-
-        def timer(t):
-            global puerta_cerrada
-            led(0)
-            mensaje = "PUERTA CERRADA!!"
-            c.publish(topic_pub, mensaje)
-            puerta_cerrada=True
-            print("puerta_cerrada") 
-            print(puerta_cerrada)
-            print("----------------------------")
-
-        tiempo.init(period=3000, mode=Timer.ONE_SHOT, callback=timer)
-
-    elif not puerta_cerrada:
-        print("\\\\\\\\\\\\PUERTA ABIERTA\\\\\\\\\\\\")
-    
-def subscriber(server):
-    global persona
-    c.set_callback(sub_cb)
-    c.connect()
-    c.subscribe(topic_sub)
-    while True:
-        c.check_msg()
-        if persona != " ":
-            abrir_cerrar(persona)
-            persona = " "
-    c.disconnect()
+#aca pensar si falla el tema del a conexion wifi
+connect.conectar_wifi()
+cerradura.subscriber(server)
