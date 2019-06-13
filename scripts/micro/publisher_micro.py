13a36bc71543e25bd9a04ad70d81f4fb2d447857
Jano <oportoalejandroa@gmail.com>
2019-06-13 14:35:31 -0300

    ahi tenes todo acomodado jajaj despues borro __pycache_

--- scripts/micro/publisher_micro.py
+++
@@ -1,7 +0,0 @@
-# Para micropython
-from umqttsimple import MQTTClient
-def main(server="169.254.254.254"):
-   c = MQTTClient("umqtt_client", server)
-   c.connect()
-   c.publish("topico/prueba", "Puerta abierta!")
-   c.disconnect()
